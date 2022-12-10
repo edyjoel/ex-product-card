9f631b1eccf148aaf6c09def82580ffd7f310462
Edy <edyxicon@gmail.com>
2022-12-10 08:59:01 -0600

    Version 0.0.1 lista

---
+++ test/data/products.ts
@@ -0,0 +1,11 @@
+export const product1 = {
+  id: '1',
+  title: 'Coffee Mug - Sin Imagen',
+  // img: './coffee-mung.png'
+}
+
+export const product2 = {
+  id: '2',
+  title: 'Coffee Mug - Con Imagen',
+  img: './coffee-mung.png'
+}
